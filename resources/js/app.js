e73a1c4c4c6830e7061f7d9d2ffb42d5db50bbec
AlexanderBaryshnikov <axr.baryshnikov@gmail.com>
2023-09-03 14:04:41 +0300

    App init

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+import './jquery.min';
+import './dz.ajax';
+import './dz.carousel';
+
+import './../vendor/wow/wow';
+import './../vendor/bootstrap/dist/js/bootstrap.bundle.min';
+import './../vendor/bootstrap-select/dist/js/bootstrap-select.min';
+import './../vendor/counter/waypoints-min';
+import './../vendor/counter/counterup.min';
+import './../vendor/swiper/swiper-bundle.min';
+import './../vendor/swiper/swiper-bundle.min';
+import './custom';
