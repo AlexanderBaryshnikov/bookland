bd9d0c0cb7de0fae7ada8d99851d63efb1ea77f6
AlexanderBaryshnikov <axr.baryshnikov@gmail.com>
2023-11-12 10:52:26 +0300

    Создан ресурс Product nova

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,19 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
     use Sluggable;
     use SoftDeletes;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'isbn',
         'name',
         'description',
         'price',
+        'author_id',
+        'publisher_id',
         'quantity',
     ];
 
@@ -28,5 +35,27 @@ class Product extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('products');
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb-products')
+            ->width(368)
+            ->height(232);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function publisher(): BelongsTo
+    {
+        return $this->belongsTo(Publisher::class);
     }
 }
